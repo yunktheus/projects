463bf50d798dddc32a37edbf2a69827a55f64690
yunktheus <theus0411@gmail.com>
2023-08-02 23:03:36 -0300

    Praticando

--- teste/teste.dart
+++ teste/teste.dart
@@ -3,34 +3,12 @@
 import 'dart:io';
 
 void main() {
-  int A, B, C, valor1, valor2, valor3, aux;
+  int A, B, SOMA;
 
   A = int.parse(stdin.readLineSync());
-
   B = int.parse(stdin.readLineSync());
 
-  C = int.parse(stdin.readLineSync());
+  SOMA = A + B;
 
-  valor1 = A;
-  valor2 = B;
-  valor3 = C;
-
-  if (B > A) {
-    aux = A;
-    A = B;
-    A = aux;
-  }
-  if (B > C) {
-    aux = C;
-    C = B;
-    B = aux;
-  }
-
-  print(A);
-  print(B);
-  print(C);
-
-  print(valor1);
-  print(valor2);
-  print(valor3);
+  print("A soma dos inteiros eh $SOMA");
 }
